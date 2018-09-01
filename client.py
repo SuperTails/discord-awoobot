ed741255a820ea304bc0a55bc41ec79e2b34a32d
ntoskrnl4 <22060947+ntoskrnl4@users.noreply.github.com>
2018-09-01 01:38:57 +0000

    almost lost all my code because I don't know how to use Git, and remove an unused import

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 # 3.7: from __future__ import annotations
-from typing import List, Callable, Dict, Tuple, Union
+from typing import List, Callable, Dict, Union
 from prefix import check_prefix
 from exceptions import UserBotError, HandlerError, TimerException
 
